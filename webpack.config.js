f5c84c1d4d980f0070736e2f0c2cd127169b7ec6
Mike Talbott <Mike.Talbott@deximaging.com>
2020-05-06 08:10:41 -0400

    Bleh webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -129,7 +129,7 @@ module.exports = {
                 runtimeCaching: [
                     {
                         urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
-                        handler: 'cacheFirst',
+                        handler: 'CacheFirst',
                         options: {
                             cacheName: 'images',
                             expiration: {
